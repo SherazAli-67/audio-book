66d5ebe2c4359207d3db0e7eb7ae783d6ff7a172
sheraz-ali <soomrosheraz054@gmail.com>
2026-08-23 16:27:29 +0500

    AudioBook app development
    1. AudioBook Detail screen UI completed

--- lib/core/app_icons.dart
+++ lib/core/app_icons.dart
@@ -6,6 +6,12 @@ class AppIcons {
   static const icBookmark ='${iconsPath}ic_bookmark.svg';
   static const icProfile ='${iconsPath}ic_profile.svg';
   static const icNotifications ='${iconsPath}ic_notifications.svg';
+  static const icMusic = '${iconsPath}ic_music.svg';
+  static const icBackward10s = '${iconsPath}ic_10s_back.svg';
+  static const icForward10s = '${iconsPath}ic_10s_forward.svg';
+  static const icPlay = '${iconsPath}ic_play.svg';
+  static const icPause = '${iconsPath}ic_pause.svg';
+  static const icRepeat = '${iconsPath}ic_repeat.svg';
 
 
   static const welcomeImg = '${iconsPath}welcome_screen_img.png';
--- lib/presentation/screens/audio_book_detail_screen.dart
+++ lib/presentation/screens/audio_book_detail_screen.dart
@@ -1,7 +1,9 @@
 import 'package:audio_book/constants/lottie_const.dart';
+import 'package:audio_book/core/app_colors.dart';
 import 'package:audio_book/core/app_icons.dart';
 import 'package:audio_book/core/app_textstyles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 
 class AudioBookDetailScreen extends StatelessWidget {
@@ -15,7 +17,6 @@ class AudioBookDetailScreen extends StatelessWidget {
         child: Padding(
           padding: const .symmetric(horizontal: 12.0, vertical: 20),
           child: Column(
-            crossAxisAlignment: .start,
             spacing: 24,
             children: [
               Row(
@@ -67,11 +68,63 @@ class AudioBookDetailScreen extends StatelessWidget {
                     )
                   ],
                 ),
-              )
+              ),
+              Padding(padding: .only(top: 30), child: Column(
+                spacing: 18,
+                children: [
+                  Row(
+                    mainAxisSize: .min,
+                    spacing: 17,
+                    children: [
+                      _buildAudioBookMetaDataWidget(icon: Icons.star_rate_rounded, title: "4.5"),
+                      _buildAudioBookMetaDataWidget(icon: Icons.language, title: "English"),
+                      _buildAudioBookMetaDataWidget(icon: Icons.mic, title: "2 hrs"),
+                    ],
+                  ),
+                  Column(
+                    spacing: 8,
+                    children: [
+                      Text("Moby Dick", style: AppTextStyles.subHeadingTextStyle,),
+                      Text("Herman Melville", style: AppTextStyles.regularTextStyle.copyWith(fontSize: 16),)
+                    ],
+                  )
+                ],
+              ),),
+              const Spacer(),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: .circular(99),
+                    color: Colors.black
+                ),
+                padding: .all(8),
+                margin: .only(bottom: 46),
+                child: Row(
+                  children: [
+                    _buildActionButton(icon: AppIcons.icMusic),
+                    _buildActionButton(icon: AppIcons.icBackward10s),
+                    IconButton(onPressed: (){}, icon: SvgPicture.asset(AppIcons.icPlay), style: IconButton.styleFrom(backgroundColor: AppColors.primaryColor, padding: .all(15)),),
+                    _buildActionButton(icon: AppIcons.icForward10s),
+                    _buildActionButton(icon: AppIcons.icRepeat),
+
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildActionButton({required String icon, VoidCallback? onTap}) => Expanded(child: IconButton(onPressed: onTap ?? (){}, icon: SvgPicture.asset(icon),));
+
+  Row _buildAudioBookMetaDataWidget({required IconData icon, required String title}) {
+    return Row(
+      spacing: 4,
+      children: [
+        Icon(icon, color: AppColors.primaryColor, size: 20,),
+        Text(title, style: AppTextStyles.regularTextStyle,)
+      ],
     );
   }
 }
